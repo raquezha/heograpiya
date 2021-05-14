248119514bf296a3b4bc6694d36fff26f38743dc
raquezha <raquezha@gmail.com>
2021-05-14 14:34:27 +0800

    Start of engine module

---
+++ engine/src/androidTest/kotlin/com/raquezha/heograpiya/engine/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package com.raquezha.heograpiya.engine
+
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.runner.RunWith
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.raquezha.heograpiya.engine.test", appContext.packageName)
+    }
+}
